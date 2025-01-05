025df83770042a5c5410c9b8c5c355b225b5fcf0
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2025-01-05 12:30:58 +0900

    Add files via upload

--- Utility/Vector/a.hpp
+++ Utility/Vector/a.hpp
@@ -21,6 +21,7 @@ template <typename T> inline operator*( const T& scalar , vector<T> v );
 template <typename T = int> inline vector<T> id( const int& size );
 
 template <typename T> inline void Sort( vector<T>& a , const bool& reversed = false );
+template <typename T0 , typename T1> inline void Sort( vector<T0>& a , vector<T1>& b , const bool& reversed = false );
 template <typename T> inline vector<int> IndexSort( const vector<T>& a , const bool& reversed = false );
 
 template <typename T , typename U = T> inline U Sum( const vector<T>& a );
--- Utility/Vector/a_Body.hpp
+++ Utility/Vector/a_Body.hpp
@@ -16,7 +16,7 @@ template <typename V> inline auto Get( V& a ) { return [&]( const int& i = 0 ) -
 template <typename T> inline vector<T> id( const int& size ) { vector<T> answer( size ); for( int i = 0 ; i < size ; i++ ){ answer[i] = i; } return answer; }
 
 template <typename T> inline void Sort( vector<T>& a , const bool& reversed ) { if( reversed ){ static auto comp = []( const T& t0 , const T& t1 ) { return t1 < t0; }; sort( a.begin() , a.end() , comp ); } else { sort( a.begin() , a.end() ); } }
-
+template <typename T0 , typename T1> inline void Sort( vector<T0>& a , vector<T1>& b , const bool& reversed ) { const int size = a.size(); assert( size == int( b.size() ) ); vector<pair<T0,T1>> v( size ); for( int i = 0 ; i < size ; i++ ){ v[i] = { move( a[i] ) , move( b[i] ) }; } Sort( v , reversed ); for( int i = 0 ; i < size ; i++ ){ a[i] = move( v[i].first ); b[i] = move( v[i].second ); } }
 template <typename T> inline vector<int> IndexSort( const vector<T>& a , const bool& reversed ) { auto index = id<int>( a.size() ); if( reversed ){ sort( index.begin() , index.end() , [&]( const int& i , const int& j ) { return a[j] < a[i]; } ); } else { sort( index.begin() , index.end() , [&]( const int& i , const int& j ) { return a[i] < a[j]; } ); } return index; }
 
 template <typename T , typename U> inline U Sum( const vector<T>& a ) { U answer{}; for( auto& x : a ){ answer += x; } return answer; }
