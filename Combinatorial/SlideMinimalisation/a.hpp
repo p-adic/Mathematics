b49908bb1486b964a3872dcb84a834479c1782fd
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2025-03-08 14:53:01 +0900

    Delete Combinatorial/SlideMinimalisation directory

--- Combinatorial/SlideMinimalisation/a.hpp
+++
@@ -1,7 +0,0 @@
-// c:/Users/user/Documents/Programming/Mathematics/Combinatorial/SlideMinimalisation/a.hpp
-
-#pragma once
-#include "a_Macro.hpp"
-
-DECLARATION_OF_SLIDE_MIMALISATION( Min );
-DECLARATION_OF_SLIDE_MIMALISATION( Max );
--- Combinatorial/SlideMinimalisation/a_Body.hpp
+++
@@ -1,7 +0,0 @@
-// c:/Users/user/Documents/Programming/Mathematics/Combinatorial/SlideMinimalisation/a_Body.hpp
-
-#pragma once
-#include "a.hpp"
-
-DEFINITION_OF_SLIDE_MIMALISATION( Min , > );
-DEFINITION_OF_SLIDE_MIMALISATION( Max , < );
