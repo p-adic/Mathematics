a94c7216bc58294c4284171b362105ca092fa517
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2025-03-08 14:46:48 +0900

    Add files via upload

---
+++ Combinatorial/SlideMinimalisation/a_Body.hpp
@@ -0,0 +1,7 @@
+// c:/Users/user/Documents/Programming/Mathematics/Combinatorial/SlideMinimalisation/a_Body.hpp
+
+#pragma once
+#include "a.hpp"
+
+DEFINITION_OF_SLIDE_MIMALISATION( Min , > );
+DEFINITION_OF_SLIDE_MIMALISATION( Max , < );
