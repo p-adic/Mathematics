0a02de81cc48b33964fd9c2564eb15eb5831e2d1
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2025-03-31 00:48:54 +0900

    Delete Arithmetic/Sum directory

--- Arithmetic/Sum/Constexpr/Log/a.hpp
+++
@@ -1,6 +0,0 @@
-// c:/Users/user/Documents/Programming/Mathematics/Arithmetic/Sum/Constexpr/Log/a.hpp
-
-#pragma once
-
-// log_2 N以上である最小の非負整数をコンパイル時計算する。
-template <typename INT> inline constexpr INT Log( INT N );
--- Arithmetic/Sum/Constexpr/Log/a_Body.hpp
+++
@@ -1,6 +0,0 @@
-// c:/Users/user/Documents/Programming/Mathematics/Arithmetic/Sum/Constexpr/Log/a_Body.hpp
-
-#pragma once
-#include "a.hpp"
-
-template <typename INT> inline constexpr INT Log( INT N ) { INT answer = 0; while( N > 1 ){ N >>= 1; answer++; } return answer; }
