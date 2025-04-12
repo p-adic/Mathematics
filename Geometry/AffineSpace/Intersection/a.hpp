bebd1ddb170b85473b3bc04f59b2c792a9ce7060
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2025-04-12 15:03:26 +0900

    Add files via upload

--- Geometry/AffineSpace/Intersection/a.hpp
+++ Geometry/AffineSpace/Intersection/a.hpp
@@ -6,11 +6,11 @@
 
 // xy平面内の線分（１点に潰れている場合も含む）上に１点が乗るか否かの判定
 template <typename INT> inline bool Online( const INT& x0 , const INT& y0 , const INT& x1 , const INT& y1 , const INT& z , const INT& w , const INT& epsilon = 0 );
-template <typename INT> inline bool Online( const pair<INT,INT>& v0 , const pair<INT,INT>& v1 , const pair<INT,INT>& v2 , const INT& epsilon = 0 );
+template <typename INT , template <typename...> typename PAIR> inline bool Online( const PAIR<INT,INT>& v0 , const PAIR<INT,INT>& v1 , const PAIR<INT,INT>& v2 , const INT& epsilon = 0 );
 
 // xy平面内の２線分（１点に潰れている場合も含む）の交差判定
 // 返り値1：交差する
 // 返り値0：接触する
 // 返り値-1：接触しない
 template <typename INT> inline int IntersectionType( const INT& x0 , const INT& y0 , const INT& x1 , const INT& y1 , const INT& z0 , const INT& w0 , const INT& z1 , const INT& w1 , const INT& epsilon = 0 );
-template <typename INT> inline int IntersectionType( const pair<INT,INT>& v0 , const pair<INT,INT>& v1 , const pair<INT,INT>& v2 , const pair<INT,INT>& v3 , const INT& epsilon = 0 );
+template <typename INT , template <typename...> typename PAIR> inline int IntersectionType( const PAIR<INT,INT>& v0 , const PAIR<INT,INT>& v1 , const PAIR<INT,INT>& v2 , const PAIR<INT,INT>& v3 , const INT& epsilon = 0 );
