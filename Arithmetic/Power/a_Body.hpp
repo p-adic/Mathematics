63d700370fe7c7e06009af11b9a2d9e8b3751fef
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2025-04-20 22:51:14 +0900

    Delete Arithmetic/Power directory

--- Arithmetic/Power/a_Body.hpp
+++
@@ -1,8 +0,0 @@
-// c:/Users/user/Documents/Programming/Mathematics/Arithmetic/Power/a_Body.hpp
-
-#pragma once
-#include "a.hpp"
-
-template <typename INT> template <typename VEC> inline PowerMemoriser<INT>::PowerMemoriser( const VEC& base ) : m_base() , m_val() { for( auto& n : base ){ m_base.push_back( n ); m_val.push_back( { 1 } ); } }
-
-template <typename INT> inline INT PowerMemoriser<INT>::Get( const int& i , const int& j ) noexcept { while( int( m_val[i].size() ) <= j ){ m_val[i].push_back( m_val[i].back() * m_base[i] ); } return m_val[i][j]; }
