046adbad5ab14870a1d471611acb16b0120650a2
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2025-04-23 08:11:18 +0900

    Add files via upload

--- Combinatorial/KnapsackProblem/Negative/Cost/a_Body.hpp
+++ Combinatorial/KnapsackProblem/Negative/Cost/a_Body.hpp
@@ -6,7 +6,7 @@
 // AbstractNegativeCostBoundedValueSumKnapsackÇ…égÇ§ÅB
 #include "Valuewise/a_Body.hpp"
 #include "../a_Body.hpp"
-#include "../../../../Arithmetic/Sum/Constexpr/Log/a_Body.hpp"
+#include "../../../../Arithmetic/Iteration/Constexpr/Log/a_Body.hpp"
 
 template <typename INT , typename U , typename COMM_MONOID> inline pair<INT,U> AbstractNegativeCostBoundedValueSumKnapsackFewValues( COMM_MONOID M , const vector<INT>& value , const INT& value_sum_bound , const vector<U>& cost , const U& cost_sum_bound , const U& exceptional ) { pair<INT,U> answer = { 0 , M.One() }; auto cost_min = AbstractNegativeCostBoundedValueSumValuewiseKnapsack( move( M ) , value , value_sum_bound , cost , exceptional , exceptional ); for( INT v = 0 ; v <= value_sum_bound ; v++ ){ auto& c_v = cost_min[v]; c_v == exceptional || cost_sum_bound < c_v ? answer : answer = { v , move( c_v ) }; } return answer; }
 template <typename INT , typename U , typename COMM_MONOID> inline pair<INT,U> AbstractNegativeCostBoundedValueSumKnapsack( COMM_MONOID M , const vector<INT>& value , const INT& value_sum_bound , const vector<U>& cost , const U& cost_sum_bound , const U& exceptional ) { const int N = value.size(); return Log( ll( value_sum_bound ) ) < ll( N >> 1 ) ? AbstractNegativeCostBoundedValueSumKnapsackFewValues( move( M ) , value , value_sum_bound , cost , cost_sum_bound , exceptional ) : AbstractNegativeBoundedValueSumKnapsack( AdditiveMonoid<INT>() , move( M ) , value , value_sum_bound , cost , cost_sum_bound ); }
--- Combinatorial/KnapsackProblem/Negative/Value/a_Body.hpp
+++ Combinatorial/KnapsackProblem/Negative/Value/a_Body.hpp
@@ -6,7 +6,7 @@
 // AbstractNegativeValueKnapsackÇ…égÇ§ÅB
 #include "Costwise/a_Body.hpp"
 #include "../a_Body.hpp"
-#include "../../../../Arithmetic/Sum/Constexpr/Log/a_Body.hpp"
+#include "../../../../Arithmetic/Iteration/Constexpr/Log/a_Body.hpp"
 
 template <typename U , typename COMM_MONOID , typename INT> inline pair<U,INT> AbstractNegativeValueKnapsackFewCosts( COMM_MONOID M , const vector<U>& value , const U& exceptional , const vector<INT>& cost , const INT& cost_sum_bound ) { pair<U,INT> answer = { M.One() , 0 }; auto value_max = AbstractNegativeValueCostwiseKnapsack( move( M ) , value , exceptional , cost , cost_sum_bound ); for( INT c = 0 ; c <= cost_sum_bound ; c++ ){ auto& v_c = value_max[c]; v_c != exceptional && ( answer.first < v_c || ( answer.first == v_c && c < answer.second ) ) ? answer = { move( v_c ) , c } : answer; } return answer; }
 template <typename U , typename COMM_MONOID , typename INT> inline pair<U,INT> AbstractNegativeValueKnapsack( COMM_MONOID M , const vector<U>& value , const U& exceptional , const vector<INT>& cost , const INT& cost_sum_bound ) { const int N = value.size(); return Log( ll( cost_sum_bound ) ) < ll( N >> 1 ) ? AbstractNegativeValueKnapsackFewCosts( move( M ) , value , exceptional , cost , cost_sum_bound ) : AbstractNegativeBoundedValueSumKnapsack( M , value , AbstractValueSumBound( M , value ) , cost , cost_sum_bound ); }
