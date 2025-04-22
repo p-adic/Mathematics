9194a371d3def21ff0ce850cbb8d2f963bde9917
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2025-04-23 08:08:23 +0900

    Delete Arithmetic/IteratedArithmetic directory

--- Arithmetic/IteratedArithmetic/DivideConquer/a.hpp
+++
@@ -1,8 +0,0 @@
-// c:/Users/user/Documents/Programming/Mathematics/Arithmetic/IteratedArithmetic/DivideConquer/a.hpp
-
-#pragma once
-
-// MがTのモノイド構造である場合にのみサポート。
-// Tの要素の列のMに関する総乗を分割統治で計算し、その結果を返す。
-// V<T>はeraseを持つ必要がある。
-template <typename T , template <typename...> typename V , typename MONOID> T DivideConquerMonoidalProd( MONOID M , V<T> f );
--- Arithmetic/IteratedArithmetic/Power/a_Body.hpp
+++
@@ -1,8 +0,0 @@
-// c:/Users/user/Documents/Programming/Mathematics/Arithmetic/IteratedArithmetic/Power/a_Body.hpp
-
-#pragma once
-#include "a.hpp"
-
-template <typename INT> inline PowerMemoriser<INT>::PowerMemoriser( const INT& base ) : m_base( base ) , m_val( 1 ) {}
-
-template <typename INT> inline INT PowerMemoriser<INT>::operator[]( const int& exponent ) noexcept { while( int( m_val.size() ) <= exponent ){ m_val.push_back( m_val.back() * m_base ); } return m_val[exponent]; }
