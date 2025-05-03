1027d663cf119ad7bce491b6599780ba826de468
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2025-05-03 15:19:35 +0900

    Delete Polynomial/FormalPowerSeries directory

--- Polynomial/FormalPowerSeries/IteratedArithmetic/a_Macro.hpp
+++
@@ -1,7 +0,0 @@
-// c:/Users/user/Documents/Programming/Mathematics/Polynomial/FormalPowerSeries/IteratedArithmetic/a_Macro.hpp
-
-#pragma once
-// DEFINITION_OF_PRODÇégÇ§ÅB
-#include "../../IteratedArithmetic/a_Macro.hpp"
-
-#define EXPRESSION3_FOR_RATIONAL_SUM { FormalPowerSeries<T>() , FormalPowerSeries<T>( 1 , Polynomial<T>::one() ) }
