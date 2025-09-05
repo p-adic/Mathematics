d047752fecb26ab50e4f1b79c5e22ccabbd929c3
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2025-09-06 07:00:27 +0900

    Add files via upload

--- Arithmetic/Mod/Function/DiscreteLog/a.hpp
+++ Arithmetic/Mod/Function/DiscreteLog/a.hpp
@@ -4,3 +4,6 @@
 
 // Baby-step giant-stepアルゴリズムで離散対数を求める。（O(√B)）
 template <typename INT1 , typename INT2 , typename INT3> int DiscreteLog( INT1 x , INT2 base , const INT3& B );
+// 一般のモノイドでは
+// ../../../Iteration/Truncated/
+// のDiscreteLogarithmを用いる。
