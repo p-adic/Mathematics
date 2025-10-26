9b23678be92c056ce449bf6d1ae8c7337dc5b2a7
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2025-10-27 08:50:06 +0900

    Add files via upload

--- Geometry/AffineSpace/Area/a.hpp
+++ Geometry/AffineSpace/Area/a.hpp
@@ -1,20 +1,17 @@
 // c:/Users/user/Documents/Programming/Mathematics/Geometry/AffineSpace/Area/a.hpp
 
 #pragma once
-#include "a_Macro.hpp"
 
 // verify:
 // https://yukicoder.me/submissions/1066871 (凸多角形)
 // https://yukicoder.me/submissions/1087031 (ヘロンの公式)
 
-// xy平面内の３点がなす三角形の符号つき面積の2倍もしくは平行四辺形の符号つき面積
-template <typename INT> inline ll Area( const INT& x0 , const INT& y0 , const INT& x1 , const INT& y1 , const INT& x2 , const INT& y2 );
-inline double Area( const double& x0 , const double& y0 , const double& x1 , const double& y1 , const double& x2 , const double& y2 );
-template <typename INT , template <typename...> typename PAIR> inline ll Area( const PAIR<INT,INT>& v0 , const PAIR<INT,INT>& v1 , const PAIR<INT,INT>& v2 );
-template <template <typename...> typename PAIR> inline double Area( const PAIR<double,double>& v0 , const PAIR<double,double>& v1 , const PAIR<double,double>& v2 );
+// xy平面内の３点がなす三角形の符号つき面積の2倍(平行四辺形の符号つき面積)
+template <typename INT> inline INT Area( const INT& x0 , const INT& y0 , const INT& x1 , const INT& y1 , const INT& x2 , const INT& y2 );
+template <typename RET , typename INT , template <typename...> typename PAIR> inline RET Area( const PAIR<INT,INT>& v0 , const PAIR<INT,INT>& v1 , const PAIR<INT,INT>& v2 );
 
 // xy平面内の３点がなす三角形の符号なし面積を辺長からヘロンの公式で求める。
 inline double Area( const double& l0 , const double& l1 , const double& l2 );
 
 // xy平面内の凸多角形の符号つき面積の２倍（反時計回りが正方向）
-template <typename INT , template <typename...> typename PAIR ,typename RET> inline RET Area( const vector<PAIR<INT,INT>>& v , RET dummy );
+template <typename RET , typename INT , template <typename...> typename PAIR> inline RET Area( const vector<PAIR<INT,INT>>& v );
--- Geometry/AffineSpace/Area/a_Body.hpp
+++ Geometry/AffineSpace/Area/a_Body.hpp
@@ -3,12 +3,9 @@
 #pragma once
 #include "a.hpp"
 
-inline ll Area_ll( const ll& x0 , const ll& y0 , const ll& x1 , const ll& y1 , const ll& x2 , const ll& y2 ) { return DEFINITION_OF_AREA; }
-template <typename INT> inline ll Area( const INT& x0 , const INT& y0 , const INT& x1 , const INT& y1 , const INT& x2 , const INT& y2 ) { return Area_ll( x0 ,y0 , x1 ,y1 , x2 ,y2 ); }
-inline double Area( const double& x0 , const double& y0 , const double& x1 , const double& y1 , const double& x2 , const double& y2 ) { return DEFINITION_OF_AREA; }
-template <typename INT , template <typename...> typename PAIR> inline ll Area( const PAIR<INT,INT>& v0 , const PAIR<INT,INT>& v1 , const PAIR<INT,INT>& v2 ) { return CALL_DEFINITION_OF_AREA; }
-template <template <typename...> typename PAIR> inline double Area( const PAIR<double,double>& v0 , const PAIR<double,double>& v1 , const PAIR<double,double>& v2 ) { return CALL_DEFINITION_OF_AREA; }
+template <typename INT> inline INT Area( const INT& x0 , const INT& y0 , const INT& x1 , const INT& y1 , const INT& x2 , const INT& y2 ) { return ( x1 - x0 ) * ( y2 - y0 ) - ( y1 - y0 ) * ( x2 - x0 ); }
+template <typename RET , typename INT , template <typename...> typename PAIR> inline RET Area( const PAIR<INT,INT>& v0 , const PAIR<INT,INT>& v1 , const PAIR<INT,INT>& v2 ) { return Area<RET>( get<0>( v0 ) , get<1>( v0 ) , get<0>( v1 ) , get<1>( v1 ) , get<0>( v2 ) , get<1>( v2 ) ); }
 
 inline double Area( const double& l0 , const double& l1 , const double& l2 ) { const double s = ( l0 + l1 + l2 ) / 2; return sqrt( s * ( s - l0 ) ) * sqrt( ( s - l1 ) * ( s - l2 ) ); }
 
-template <typename INT , template <typename...> typename PAIR ,typename RET> inline RET Area( const vector<PAIR<INT,INT>>& v , RET dummy ) { const int& size = v.size(); assert( size > 2 ); for( int i = 2 ; i < size ; i++ ){ dummy += Area( v[0] , v[i-1] , v[i] ); } return move( dummy ); }
+template <typename RET , typename INT , template <typename...> typename PAIR> inline RET Area( const vector<PAIR<INT,INT>>& v ) { RET answer{}; const int& size = v.size(); assert( size > 2 ); for( int i = 2 ; i < size ; i++ ){ answer += Area<RET>( v[0] , v[i-1] , v[i] ); } return answer; }
