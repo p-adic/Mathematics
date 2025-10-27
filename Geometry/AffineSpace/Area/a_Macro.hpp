d1201ab73b9994dd5e1f29b8dc30e75747f0fc40
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2025-10-27 19:58:52 +0900

    Delete Geometry/AffineSpace/Area/a_Macro.hpp

--- Geometry/AffineSpace/Area/a_Macro.hpp
+++
@@ -1,6 +0,0 @@
-// c:/Users/user/Documents/Programming/Mathematics/Geometry/AffineSpace/a_Macro.hpp
-
-#pragma once
-#define DEFINITION_OF_AREA ( x1 - x0 ) * ( y2 - y0 ) - ( y1 - y0 ) * ( x2 - x0 )
-#define CALL_DEFINITION_OF_AREA Area( get<0>( v0 ) , get<1>( v0 ) , get<0>( v1 ) , get<1>( v1 ) , get<0>( v2 ) , get<1>( v2 ) )
-
