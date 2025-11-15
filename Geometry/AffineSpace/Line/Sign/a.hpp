568e34ab9f97796c4384e78f9455425602e07992
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2025-11-16 08:21:02 +0900

    Add files via upload

---
+++ Geometry/AffineSpace/Line/Sign/a.hpp
@@ -0,0 +1,5 @@
+// c:/Users/user/Documents/Programming/Mathematics/Geometry/AffineSpace/Line/Sign/a.hpp
+
+#pragma once
+
+template <typename INT> inline int sign( const INT& n , const INT& epsilon = 0 );
