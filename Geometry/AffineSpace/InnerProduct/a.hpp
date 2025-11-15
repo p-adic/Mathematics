ac664807f459ca41afe5539c035dc6f3b558d339
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2025-11-16 08:22:29 +0900

    Delete Geometry/AffineSpace/InnerProduct directory

--- Geometry/AffineSpace/InnerProduct/a.hpp
+++
@@ -1,8 +0,0 @@
-// c:/Users/user/Documents/Programming/Mathematics/Geometry/AffineSpace/InnerProduct/a.hpp
-
-#pragma once
-
-template <typename INT , template <typename...> typename PAIR> INT InnerProduct( const PAIR<INT,INT>& v0 , const PAIR<INT,INT>& v1 );
-template <typename INT> INT InnerProduct( const tuple<INT,INT,INT>& v0 , const tuple<INT,INT,INT>& v1 );
-template <typename INT> INT InnerProduct( const tuple<INT,INT,INT,INT>& v0 , const tuple<INT,INT,INT,INT>& v1 );
-template <typename INT> INT InnerProduct( const vector<INT>& v0 , const vector<INT>& v1 );
