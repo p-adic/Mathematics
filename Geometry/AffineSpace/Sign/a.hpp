5703ca22c24cf852024b9de348f0ad6b09d9455f
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2025-11-16 08:20:40 +0900

    Delete Geometry/AffineSpace/Sign directory

--- Geometry/AffineSpace/Sign/a.hpp
+++
@@ -1,5 +0,0 @@
-// c:/Users/user/Documents/Programming/Mathematics/Geometry/AffineSpace/Sign/a.hpp
-
-#pragma once
-
-template <typename INT> inline int sign( const INT& n , const INT& epsilon = 0 );
--- Geometry/AffineSpace/Sign/a_Body.hpp
+++
@@ -1,6 +0,0 @@
-// c:/Users/user/Documents/Programming/Mathematics/Geometry/AffineSpace/Sign/a_Body.hpp
-
-#pragma once
-#include "a.hpp"
-
-template <typename INT> inline int sign( const INT& n , const INT& epsilon ) { constexpr bool is_not_double = !is_same_v<INT,double>; assert( is_not_double || epsilon > 0 ); return n < -epsilon ? -1 : epsilon < n ? 1 : 0; }
