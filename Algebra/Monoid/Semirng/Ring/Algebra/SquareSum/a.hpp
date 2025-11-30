ddea71e0f567813227f8e7d5b24eb54482a945e4
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2025-11-30 15:49:17 +0900

    Add files via upload

--- Algebra/Monoid/Semirng/Ring/Algebra/SquareSum/a.hpp
+++ Algebra/Monoid/Semirng/Ring/Algebra/SquareSum/a.hpp
@@ -8,20 +8,20 @@
 // (1) RがUの非可換環構造を定める。
 // を満たす場合にのみサポート。
 
-// Uの要素からなる多重集合に対する(要素数,総和,二乗和の総和)を管理するU代数構造。
+// Uの要素からなる多重集合に対する(要素数,総和,二乗和)を管理するU代数構造。
 // 多重集合S,Tに対しS cup Tに相当する和と、S+Tに相当する積が定義される。
 
 // VirtualAlgebraで抽象化すると加法群、乗法モノイドそれぞれクラスとして定義しないと
 // いけないので抽象化しない。
 template <typename RING , typename U = inner_t<RING>>
-class SquareAlgebra
+class SquareSumAlgebra
 {
 
 private:
   RING m_R;
 
 public:
-  inline SquareAlgebra( RING R );
+  inline SquareSumAlgebra( RING R );
   inline const T3<U>& Zero();
   inline const T3<U>& One();
   inline T3<U> Sum( T3<U> v0 , const T3<U>& v1 );
