1e26ae75cfee051378d7abc9b25154fc924031c5
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2025-11-30 20:20:57 +0900

    Add files via upload

--- SetTheory/Line/CoordinateCompress/RightValue/a_Body.hpp
+++ SetTheory/Line/CoordinateCompress/RightValue/a_Body.hpp
@@ -3,13 +3,13 @@
 #pragma once
 #include "a.hpp"
 
-template <typename INT , bool ordered> inline CoordinateCompressR<INT>::CoordinateCompressR() : m_r() {}
+template <typename INT , bool ordered> inline CoordinateCompressR<INT,ordered>::CoordinateCompressR() : m_r() {}
 
-template <typename INT , bool ordered> inline void CoordinateCompressR<INT>::Set( INT t ) { m_r.insert( move( t ) ); }
-template <typename INT , bool ordered> template <typename U , template <typename...> typename V > inline void CoordinateCompressR<INT>::Set( V<U> a ) { for( auto& t : a ){ Set( move( t ) ); } }
+template <typename INT , bool ordered> inline void CoordinateCompressR<INT,ordered>::Set( INT t ) { m_r.insert( move( t ) ); }
+template <typename INT , bool ordered> template <typename U , template <typename...> typename V > inline void CoordinateCompressR<INT,ordered>::Set( V<U> a ) { for( auto& t : a ){ Set( move( t ) ); } }
 
 template <typename INT , bool ordered>
-tuple<vector<INT>,conditional_t<ordered,map<INT,int>,unordered_map<INT,int>>,int> CoordinateCompressR<INT>::Get()
+tuple<vector<INT>,conditional_t<ordered,map<INT,int>,unordered_map<INT,int>>,int> CoordinateCompressR<INT,ordered>::Get()
 {
 
   tuple<vector<INT>,conditional_t<ordered,map<INT,int>,unordered_map<INT,int>>,int> answer{};
@@ -26,5 +26,5 @@ tuple<vector<INT>,conditional_t<ordered,map<INT,int>,unordered_map<INT,int>>,int
 
 }
 
-template <typename INT , bool ordered> inline void CoordinateCompressR<INT>::clear() { m_r.clear(); }
+template <typename INT , bool ordered> inline void CoordinateCompressR<INT,ordered>::clear() { m_r.clear(); }
 
