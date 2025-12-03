fbf77cf98ed2d68696d49d41a187c16279109357
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2025-12-03 23:50:17 +0900

    Add files via upload

--- Arithmetic/Mod/Function/Residue/Quotient/a.hpp
+++ Arithmetic/Mod/Function/Residue/Quotient/a.hpp
@@ -2,11 +2,13 @@
 
 #pragma once
 
+// O(log(min(base,abs(c))+1))で
 // ca \equiv gcd(c,base) mod baseを満たすaとgcdの組(a,gcd)を返す。
 // ModularInverseとはassertと戻り値だけ異なる関数だが、
 // ModularInverseは定数倍高速化のためこの関数を経由させない。
-template <typename INT> pair<INT,ll> CoprimeFactorInverse( const ll& base , INT c );
+template <typename INT> pair<INT,ll> CoprimeFactorInverse( const ll& base , const INT& c );
 
+// O(log(min(base,abs(d))+1))で
 // n \equiv da mod baseを満たす0<=a<baseが存在するならばその最小値を返し、
 // 存在しないならば-1を返す。
 template <typename INT> inline INT ModularQuotient( const ll& base , const INT& n , const ll& d );
--- Arithmetic/Mod/Function/Residue/Quotient/a_Body.hpp
+++ Arithmetic/Mod/Function/Residue/Quotient/a_Body.hpp
@@ -3,27 +3,10 @@
 #pragma once
 #include "a.hpp"
 
-template <typename INT>
-pair<INT,ll> CoprimeFactorInverse( const ll& base , INT c )
-{
+#include "../a_Body.hpp"
+#include "../../CRT/a_Body.hpp"
 
-  assert( base > 0 );
-  ll a[2] = { 0 , 1 % base };
-  INT b[2] = { base , INT( ( c %= base ) < 0 ? c += base : c ) };
-
-  while( b[1] != 0 ){
-
-    const INT q = b[0] / b[1];
-    ( a[0] -= q * a[1] % base ) < 0 ? a[0] += base : a[0];
-    b[0] -= q * b[1];
-    swap( a[0] , a[1] );
-    swap( b[0] , b[1] );
-
-  }
-
-  return { move( a[0] ) , move( b[0] ) };
-
-}
+template <typename INT> inline pair<INT,ll> CoprimeFactorInverse( const ll& base , const INT& c ) { assert( base > 0 ); auto [u_0,u_1,g] = PartitionOfUnity<ll,ll>( base, c ); return { Residue( u_1 , base ) , move( g ) }; }
 
 template <typename INT> inline INT ModularQuotient( const ll& base , const INT& n , const ll& d , const ll& coef , const ll& gcd ) { assert( 0 <= coef && coef < base && ( d % base * coef - gcd ) % base == 0 ); return n % gcd == 0 ? Residue( n / gcd % base * coef , base / gcd ) : -1; }
 template <typename INT> inline INT ModularQuotient( const ll& base , const INT& n , const ll& d ) { auto [coef,gcd] = CoprimeFactorInverse( base , d ); return ModularQuotient( base , n , d , coef ,gcd ); }
