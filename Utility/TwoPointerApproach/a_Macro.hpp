6f333c6c692d60303cf97c667028acb4d4029fd6
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2025-12-07 22:14:20 +0900

    Add files via upload

--- Utility/TwoPointerApproach/a_Macro.hpp
+++ Utility/TwoPointerApproach/a_Macro.hpp
@@ -20,7 +20,9 @@
 // 例えば長さNの非負整数値配列Aで極大な正値区間とそこでの総和を取得したい場合
 // auto update_L = [&]( int& i_L , auto& i_info ){ i_info -= A[i_L++]; };
 // auto update_R = [&]( int& i_R , auto& i_info ){ if( ++i_R < N ){ i_info += A[i_R]; } };
-// TPA( interval , i , 0 , i_R < N , update_L( i_L , i_info ) , update_R( i_R , i_info ) , A[i_L] > 0 && A[i_R] > 0 , ll( A[0] ) );
+// auto on = [&]( const int& i_L , const int& i_R ){ return A[i_L] > 0 && A[i_R] > 0; }
+// ll i_info_init = A[0];
+// TPA( interval , i , 0 , i_R < N , update_L( i_L , i_info ) , update_R( i_R , i_info ) , on( i_L , i_R ) , i_info_init );
 // とすればtuple<int,int,ll>値配列intervalに{左端,右端,総和}の列が格納される。
 // VAR_TPA ## _infoもintervalにコピーされるので、setやvectorなどのコピーのコストが
 // 大きいデータを用いてon,off判定する時はTPAより前に宣言して使う。
