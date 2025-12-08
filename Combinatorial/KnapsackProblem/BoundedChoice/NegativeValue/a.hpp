400fefa49c9c8e92f4775a89dba93de49865b6d9
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2025-12-08 23:59:27 +0900

    Add files via upload

--- Combinatorial/KnapsackProblem/BoundedChoice/NegativeValue/a.hpp
+++ Combinatorial/KnapsackProblem/BoundedChoice/NegativeValue/a.hpp
@@ -13,7 +13,7 @@
 // コストの総和0 <= c <= cost_sum_boundごとに価値の総和の最大値を求める。
 template <typename U , typename N_MODULE , typename INT1 , typename INT2> inline vector<U> AbstractNegativeValueBoundedChoiceCostwiseKnapsack( N_MODULE M , const vector<U>& value , const U& exceptional , const vector<INT1>& cost , const INT1& cost_sum_bound , const vector<INT2>& choice_num_bound );
 template <typename INT1 , typename INT2 , typename INT3> inline vector<INT1> NegativeValueBoundedChoiceCostwiseKnapsack( const vector<INT1>& value , const INT1& exceptional , const vector<INT1>& cost , const INT2& cost_sum_bound , const vector<INT3>& choice_num_bound );
-// 計算量O(N min(cost_sum_bound,cn_max 2^{N/2}) log_2 cn_max)で
+// 計算量O(N min(cost_sum_bound,cn_max^{N/2}) log_2 cn_max)で
 // 各iに対し項目iをchoice_num_bound(i)回まで重複選択できるとして、コストの総和cost_sum_bound
 // 以下における価値の総和の最大値とそれを実現するコストの最小値を求める。
 template <typename U , typename N_MODULE , typename INT1 , typename INT2> inline pair<U,INT1> AbstractNegativeValueBoundedChoiceKnapsack( N_MODULE M , const vector<U>& value , const U& exceptional , const vector<INT1>& cost , const INT1& cost_sum_bound , const vector<INT2>& choice_num_bound );
