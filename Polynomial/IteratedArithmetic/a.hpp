31060ef2410e6c4a1b504f3cb1db85dcea175d72
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2026-01-25 16:06:15 +0900

    Add files via upload

--- Polynomial/IteratedArithmetic/a.hpp
+++ Polynomial/IteratedArithmetic/a.hpp
@@ -1,20 +1,30 @@
 // c:/Users/user/Documents/Programming/Mathematics/Polynomial/IteratedArithmetic/a.hpp
 
 #pragma once
+// verify:
+// https://yukicoder.me/submissions/1141316 (Prod)
+
 #include "a_Macro.hpp"
 #include "../a.hpp"
 
-// 以下D = 次数の上限、L = fの長さとする。
+// 以下L = fの長さ、D_sum = 次数の総和とする。
 
-// 多項式の総和(O(DL))
-template <typename T , template <typename...> typename V> Polynomial<T>& Sum( V<Polynomial<T>>& f );
+// 多項式の総和は
+// ../../Arithmetic/Iteration/
+// のSumで良い。
 
-// 有理式の総和（O(DL(log DL)^2)）
-template <typename T , template <typename...> typename V> pair<Polynomial<T>,Polynomial<T>>& RationalSum( V<pair<Polynomial<T>,Polynomial<T>>>& f );
+// 次数Nで打ち切る場合は
+// ../FPS/Arithmetic/Iteration/
+// を用いる。
 
-// 多項式の総乗（O(DL(log DL)^2)）
-template <typename T , template <typename...> typename V> Polynomial<T>& Prod( V<Polynomial<T>>& f );
+// 有理式の総和（O(D_sum(log D_sum)(log L))）
+template <typename T> pair<Polynomial<T>,Polynomial<T>> RationalSum( list<pair<Polynomial<T>,Polynomial<T>>> f );
 
-// 多項式のe乗（O(De(log De)^2)）
+// 多項式の総乗（O(D_sum(log D_sum)(log L))）
+template <typename T> Polynomial<T> Prod( list<Polynomial<T>> f );
+
+
+// 以下D = 次数の上限とする。
+
+// 多項式のe乗（O(De(log De)(log e))）
 template <typename T> inline Polynomial<T> Power( const Polynomial<T>& f , const int& e );
-
