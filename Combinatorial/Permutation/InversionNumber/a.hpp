fa2dbdd9e0488dfe620f70677033633e52d13a74
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2026-03-06 23:47:17 +0900

    Add files via upload

--- Combinatorial/Permutation/InversionNumber/a.hpp
+++ Combinatorial/Permutation/InversionNumber/a.hpp
@@ -4,8 +4,9 @@
 // verify:
 // https://yukicoder.me/submissions/1030881（InversionNumber）
 
-// 順列とは限らない配列Aの転倒数計算O(N log N)
+// 順列とは限らない非負整数値配列Aの転倒数計算O(Max(A)+N log Max(A))
 template <typename INT> ll InversionNumber( const vector<INT>& A );
+// Max(A)が大きい場合は座標圧縮を併用する。
 
 // 順列Pの極小基本互換表示の濃度計算O(N log N)
 template <typename INT> inline ll LeastAdjacentSwapCount( const vector<INT>& P );
