dc8edfa43e75ec48c214ce37173ef09df98c405a
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2026-08-30 18:26:24 +0900

    Add files via upload

--- Geometry/Graph/Acyclic/LongestWalk/a.hpp
+++ Geometry/Graph/Acyclic/LongestWalk/a.hpp
@@ -3,12 +3,19 @@
 #pragma once
 
 // verify:
-// https://yukicoder.me/submissions/972197（GetLongestWalk）
+// https://yukicoder.me/submissions/972197 (始点／終点指定なし)
+// https://yukicoder.me/submissions/1139133 (始点／終点指定あり)
 
 // 入力の範囲内で要件
 // (1) Gが辺Edge:T->(T \times U)^{< \omega}を持ち閉路を持たない有向グラフである。
 // (2) Mがoperator<(const U&,const U&)に関してUの順序モノイド構造である。
+// (3) externalは経路長に現れない。
 // を満たす場合にのみサポート。
+
+// t_startsのいずれかの頂点を始点としt_finalsのいずれかの頂点を終点とするGの最長歩道を
+// O(|V_G|+|E_G|)で計算する。存在しない場合は{external,{}}を返す。
+template <typename ACYCLIC_GRAPH , typename MONOID , typename T = inner_t<ACYCLIC_GRAPH> , typename U = inner_t<MONOID>> pair<U,vector<T>> GetLongestWalk( ACYCLIC_GRAPH& G , MONOID M , const vector<T>& t_starts , const vector<T>& t_finals , U external );
+
 // Gの最長歩道をO(|V_G|+|E_G|)で計算する。
-template <typename ACYCLIC_GRAPH , typename MONOID> pair<inner_t<MONOID>,list<inner_t<ACYCLIC_GRAPH>>> GetLongestWalk( ACYCLIC_GRAPH& G , MONOID M );
+template <typename ACYCLIC_GRAPH , typename MONOID , typename U , typename T = inner_t<ACYCLIC_GRAPH>> inline pair<U,vector<T>> GetLongestWalk( ACYCLIC_GRAPH& G , MONOID M , U external );
 // Gが有向木の場合はDepthFirstSearchOnTree::Depthの最大値で良い。
--- Geometry/Graph/Acyclic/LongestWalk/a_Body.hpp
+++ Geometry/Graph/Acyclic/LongestWalk/a_Body.hpp
@@ -4,41 +4,45 @@
 #include "a.hpp"
 
 #include "../a_Body.hpp"
+#include "../../../../Utility/Vector/a_Body.hpp"
 
-
-template <typename ACYCLIC_GRAPH , typename MONOID> pair<inner_t<MONOID>,list<inner_t<ACYCLIC_GRAPH>>> GetLongestWalk( ACYCLIC_GRAPH& G , MONOID M )
+template <typename ACYCLIC_GRAPH , typename MONOID , typename T , typename U> pair<U,vector<T>> GetLongestWalk( ACYCLIC_GRAPH& G , MONOID M , const vector<T>& t_starts , const vector<T>& t_finals , U external )
 {
 
-  using T = inner_t<ACYCLIC_GRAPH>;
-  using U = inner_t<MONOID>;
+  static_assert( is_same_v<T,inner_t<ACYCLIC_GRAPH>> );
+  static_assert( is_same_v<U,inner_t<MONOID>> );
   const int& size = G.size();
   const U& one = M.One();
-
-  if( size == 0 ){
-
-    return { one , list<T>() };
-
-  }
-  
   vector<T> top_sort = TopologicalSort( G );
-  vector<U> dp( size , one );
+  vector<U> dp( size , external );
   vector<int> prev( size , -1 );
 
-  for( auto itr_vertex = top_sort.begin() , end_vertex = top_sort.end() ; itr_vertex != end_vertex ; itr_vertex++ ){
+  for( auto& v : t_starts ){
 
-    auto&& edge_i = G.Edge( *itr_vertex );
-    auto&& i = G.Enumeration_inv( *itr_vertex );
-    const U& dp_i = dp[i];
+    auto&& i = G.Enumeration_inv( v );
+    dp[i] = one;
+    
+  }
 
-    for( auto itr_edge_i = edge_i.begin() , end_edge_i = edge_i.end() ; itr_edge_i != end_edge_i ; itr_edge_i++ ){
-      auto& [t,u] = *itr_edge_i;
+  for( auto& v : top_sort ){
+
+    auto&& i = G.Enumeration_inv( v );
+
+    if( dp[i] == external ){
+
+      continue;
+
+    }
+
+    for( auto& [t,u] : G.Edge( v ) ){
+
       auto&& j = G.Enumeration_inv( t );
-      U& dp_j = dp[j];
+      auto nu = M.Product( dp[i] , u );
 
-      if( !( dp_i < dp_j ) ){
+      if( dp[j] == external || dp[j] < nu ){
 
-	dp_j = M.Product( dp_i , u );
-	prev[j]	= i;
+	dp[j] = nu;
+	prev[j] = i;
 
       }
 
@@ -46,31 +50,33 @@ template <typename ACYCLIC_GRAPH , typename MONOID> pair<inner_t<MONOID>,list<in
 
   }
 
-  U answer = one;
-  int end_num = -1;
+  int j = -1;
 
-  for( int i = 0 ; i < size ; i++ ){
+  for( auto& t : t_finals ){
 
-    const U& dp_i = dp[i];
+    auto&& i = G.Enumeration_inv( t );
 
-    if( end_num == -1 || answer < dp_i ){
-      
-      answer = dp_i;
-      end_num = i;
+    if( dp[i] != external && ( j == -1 || dp[j] < dp[i] ) ){
+
+      j = i;
 
     }
+    
+  }
+
+  int end_num = j;
+  vector<T> path{};
+
+  while( j != -1 ){
+
+    path <<= G.Enumeration( j );
+    j = prev[j];
 
   }
 
-  list<T> path{};
-
-  while( end_num != -1 ){
-
-    path.push_front( G.Enumeration( end_num ) );
-    end_num = prev[end_num];
-
-  }
-
-  return { move( answer ) , move( path ) };
+  Reverse( path );
+  return { move( end_num == -1 ? external : dp[end_num] ) , move( path ) };
 
 }
+
+template <typename ACYCLIC_GRAPH , typename MONOID , typename U , typename T> inline pair<U,vector<T>> GetLongestWalk( ACYCLIC_GRAPH& G , MONOID M , U external ) { const int& size = G.size(); vector<T> t_starts( size ); for( int i = 0 ; i < size ; i++ ){ t_starts[i] = G.Enumeration( i ); } return GetLongestWalk( G , move( M ) , t_starts , t_starts , move( external ) ); }
