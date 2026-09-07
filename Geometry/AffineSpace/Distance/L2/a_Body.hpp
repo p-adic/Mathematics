4f3b35c051d643e3700f851435b95990fd7b2be1
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2026-09-08 00:12:23 +0900

    Add files via upload

--- Geometry/AffineSpace/Distance/L2/a_Body.hpp
+++ Geometry/AffineSpace/Distance/L2/a_Body.hpp
@@ -7,6 +7,7 @@
 #include "../../../../Utility/Tuple/a_Body.hpp"
 
 template <typename INT> inline INT L22( const INT& x , const INT& y ) { return x * x + y * y; }
+inline ll L22( const int& x , const int& y ) { return L22( ll{ x } , ll{ y } ); }
 template <typename INT , template <typename...> typename PAIR> inline INT L22( const PAIR<INT,INT>& v ) { return L22( get<0>( v ) , get<1>( v ) ); }
 
 template <typename INT> inline INT L22_Distance( const INT& x0 , const INT& y0 , const INT& x1 , const INT& y1 ) { return L22( x0 - x1 , y0 - y1 ); }
