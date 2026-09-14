46bf0aa09a7be6e0bf7c81e77edbce0faf07f24a
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2026-09-14 18:03:31 +0900

    Add files via upload

--- Combinatorial/KnapsackProblem/Negative/Value/Multicost/a.hpp
+++ Combinatorial/KnapsackProblem/Negative/Value/Multicost/a.hpp
@@ -1,32 +1,20 @@
 // c:/Users/user/Documents/Programming/Mathematics/Combinatorial/KnapsackProblem/Negative/Value/Multicost/a.hpp
 
 #pragma once
-// verify:
-// NegativeValueMulticostKnapsack（N,W,D,L）
-// https://yukicoder.me/submissions/976638（5e3,5e3,2,1）
-
-
-// 以下cost_sum_boundのサイズをDと置き、D>=2とする。
-// max_{d=0}^{D-1} cost_sum_bound[d]をWと置く。
-// max_c valid(c).size()をLと置く。
-// VALIDは関数valid:N^{D-1} -> {(l,r) in N^2|l<=r}^{< omega}に相当する型。
 
 // 入力の範囲内で要件
 // (1) Mはoperator<(const U&,const U&)に関してUの全順序可換モノイド構造である。
 // (2) exceptionalは価値の総和で表せない。
-// (3) N未満の各iに対しcord[i]が0以上D未満である。
-// (4) N未満の各iに対しcost[i]が0以上である。
-// (5) D未満の各dに対し0 <= cost_sum_bound[d]である。
-// (6) (0,r) in valid(0,...,0)を満たすr in Nが存在する
+// (3) N未満の各iに対しcost0[i]が0以上である。
+// (4) N未満の各iに対しcost1[i]が0以上である。
+// (5) 0 <= cost_sum0_boundである。
+// (6) 0 <= cost_sum1_boundである。
 // を満たす場合にのみサポート。
-// 計算量O(N W
-//   + sum_{c in prod_{d=0}^{D-2} [0,cost_sum_bound[d]]}
-//     (D + valid(c).size() log (1+cost_sum_bound[D-1])))
-// 特にO(N W + W^{D-1}(D + L log (1+W)))で
-// N未満の各iに対し項目iのコストをcost[i]*delta_{coord[i]} in N^Dとした時
-// コストの総和(c_0,...,c_{D-1})がprod_d cost_sum_bound[d]に入りかつ
-// valid(c_0,...c_{D-2})の定める区間列の和集合にc_{D-1}が属す制約下における
-// 価値の総和の最大値を求める。
-template <typename U , typename COMM_MONOID , typename INT1 , typename INT2 , typename VALID> U AbstractNegativeValueMulticostKnapsack( COMM_MONOID M , const vector<U>& value , const U& exceptional , const vector<INT1>& coord , const vector<INT2>& cost , const vector<INT2>& cost_sum_bound , VALID valid );
-template <typename INT1 , typename INT2 , typename INT3 , typename VALID> inline INT1 NegativeValueMulticostKnapsack( const vector<INT1>& value , const INT1& exceptional , const vector<INT2>& coord , const vector<INT3>& cost , const vector<INT3>& cost_sum_bound , VALID valid );
-
+// 計算量O(N cost0_sum_bound cost1_sum_bound)で
+// N未満の各iに対し項目iのコストを(cost0[i],cost1[i]) in N^2とした時
+// コストの総和(c_0,c_1)が
+// - c_0 <= cost0_sum_bound
+// - c_1 <= cost1_sum_bound
+// を満たす時の価値の総和の最大値を求める。
+template <typename U , typename COMM_MONOID> U AbstractNegativeValueMulticostKnapsack( COMM_MONOID M , const vector<U>& value , const U& exceptional , const vector<int>& cost0 , const int& cost0_sum_bound , const vector<int>& cost1 , const int& cost1_sum_bound );
+template <typename INT> inline int NegativeValueMulticostKnapsack( const vector<INT>& value , const INT& exceptional , const vector<int>& cost0 , const int& cost0_sum_bound , const vector<int>& cost1 , const int& cost1_sum_bound );
--- Combinatorial/KnapsackProblem/Negative/Value/Multicost/a_Body.hpp
+++ Combinatorial/KnapsackProblem/Negative/Value/Multicost/a_Body.hpp
@@ -3,81 +3,40 @@
 #pragma once
 #include "a.hpp"
 
-#include "../Costwise/a_Body.hpp"
-#include "../../../../Loop/a_Body.hpp"
-#include "../../../../../SetTheory/DirectProduct/AffineSpace/BIT/IntervalMax/a_Body.hpp"
+#include "../../../../../Arithmetic/Iteration/a_Body.hpp"
 
-template <typename U , typename COMM_MONOID , typename INT , typename VALID> U AbstractNegativeValueMulticostKnapsack( COMM_MONOID M , const vector<vector<U>>& value , const U& exceptional , const vector<vector<INT>>& cost , const vector<INT>& cost_sum_bound , VALID valid )
+template <typename U , typename COMM_MONOID>
+U AbstractNegativeValueMulticostKnapsack( COMM_MONOID M , const vector<U>& value , const U& exceptional , const vector<int>& cost0 , const int& cost0_sum_bound , const vector<int>& cost1 , const int& cost1_sum_bound )
 {
 
-  static_assert( is_invocable_r_v<vector<pair<INT,INT>>,VALID,const vector<INT>&> );
-  const int D = cost_sum_bound.size();
-  assert( D >= 2 );
-  vector value_max( D , vector<U>() );
+  const int N = value.size();
+  assert( N == int( cost0.size() ) && N == int( const1.size() ) );
+  vector dp( cost0_sum_bound + 1 , vector( cost1_sum_bound + 1 , exceptional ) );
+  U answer = dp[0][0] = M.One();
+  
+  for( int i = 0 ; i < N ; i++ ){
 
-  for( int d = 0 ; d < D ; d++ ){
+    for( int w0 = cost0_sum_bound ; w0 >= cost0[i] ; w0-- ){
 
-    value_max[d] = AbstractNegativeValueCostwiseKnapsack( M , value[d] , exceptional , cost[d] , cost_sum_bound[d] );
+      for( int w1 = cost1_sum_bound ; w1 >= cost1[i] ; w1-- ){
 
-  }
+        U temp = M.Product( dp[w0-cost0[i]][w1-cost1[i]] , value[i] );
+        
+        if( dp[w0][w1] == exceptional || dp[w0][w1] < temp ){
 
-  const U& one = M.One();
-  const int D_minus = D - 1;
-  IntervalMaxBIT bit{ one , value_max[D_minus] };
-  vector<INT> index( D_minus , 0 ) , lower_bound = index;
+          SetMax( answer , dp[w0][w1] = move( temp ) );
 
-  U answer = one;
-  bool b = true;
-
-  while( b ){
-
-    U temp = one;
-
-    for( int d = 0 ; d < D_minus ; d++ ){
-
-      value_max[d][index[d]] == exceptional ? temp = exceptional : temp = M.Product( move( temp ) , value_max[d][index[d]] );
-
-    }
-
-    if( temp != exceptional ){
-      
-      for( auto& [l,r] : valid( index ) ){
-
-	assert( l <= r );
-	answer = max( answer , M.Product( bit.IntervalMax( l , r ) , temp ) );
+        }
 
       }
 
     }
 
-    b = NextLoopEq( D_minus , lower_bound , cost_sum_bound , index );
-    
   }
 
   return answer;
 
 }
 
-template <typename U , typename COMM_MONOID , typename INT1 , typename INT2 , typename VALID> U AbstractNegativeValueMulticostKnapsack( COMM_MONOID M , const vector<U>& value , const U& exceptional , const vector<INT1>& coord , const vector<INT2>& cost , const vector<INT2>& cost_sum_bound , VALID valid )
-{
 
-  const int N = value.size();
-  const INT1 D = cost_sum_bound.size();
-  assert( int( coord.size() ) == N && int( cost.size() ) == N );
-  vector value_copy( D , vector<U>() );
-  vector cost_copy( D , vector<INT2>() );
-
-  for( int i = 0 ; i < N ; i++ ){
-
-    auto& coord_i = coord[i];
-    assert( 0 <= coord_i && coord_i < D );
-    value_copy[coord_i].push_back( value[i] );
-    cost_copy[coord_i].push_back( cost[i] );
-
-  }
-
-  return AbstractNegativeValueMulticostKnapsack( move( M ) , value_copy , exceptional , cost_copy , cost_sum_bound , move( valid ) );
-
-}
-
-template <typename INT1 , typename INT2 , typename INT3 , typename VALID> inline INT1 NegativeValueMulticostKnapsack( const vector<INT1>& value , const INT1& exceptional , const vector<INT2>& coord , const vector<INT3>& cost , const vector<INT3>& cost_sum_bound , VALID valid ) { return AbstractNegativeValueMulticostKnapsack( AdditiveMonoid<INT1>() , value , exceptional , coord , cost , cost_sum_bound , move( valid ) ); }
+template <typename INT> inline int NegativeValueMulticostKnapsack( const vector<INT>& value , const INT& exceptional , const vector<int>& cost0 , const int& cost0_sum_bound , const vector<int>& cost1 , const int& cost1_sum_bound ) { return AbstractNegativeValueMulticostKnapsack( AdditiveMonoid<INT>() , value , exceptional , cost0 , cost0_sum_bound , cost1 , cost1_sum_bound ); }
